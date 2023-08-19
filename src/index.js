c456e29936c91a9935c181f06e3827ec7dafc5f1
Artur Mochnia <amochnia@gmail.com>
2023-08-19 10:22:05 +0100

    added index src

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
-import { theme } from "./theme";
-
-
+import App from './core/App';
+import { Provider } from 'react-redux';
+import store from './features/personalHomepage/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+  <StrictMode>
+    <Provider store={store}>
       <App />
-    </ThemeProvider>
-    
-  </React.StrictMode>
+    </Provider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
