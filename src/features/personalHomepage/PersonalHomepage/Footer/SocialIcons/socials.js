5d624e73077babb854c12723ac88bdc920892c39
Artur Mochnia <amochnia@gmail.com>
2023-08-20 10:05:11 +0100

    added LinkedIn

--- src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,7 +1,7 @@
 import { styleIcon } from "./styled";
 import { ReactComponent as GithubIcon } from "./icons/github.svg";
 // import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
-// import { ReactComponent as linkedInIcon } from "./icons/linkedin.svg";
+import { ReactComponent as linkedInIcon } from "./icons/linkedin.svg";
 // import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
 
 export const socials = [
@@ -11,18 +11,18 @@ export const socials = [
     Icon: styleIcon(GithubIcon),
   },
   // {
-    //name: "Facebook",
-    //url: "http://facebook.com",
-    //Icon: styleIcon(FacebookIcon),
+  //name: "Facebook",
+  //url: "http://facebook.com",
+  //Icon: styleIcon(FacebookIcon),
   //},
-  // {
-    //name: "LinkedIn",
-    //url: "http://linkedin.com",
-    //Icon: styleIcon(linkedInIcon),
-  //},
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/artur-mochnia-070a8184/",
+    Icon: styleIcon(linkedInIcon),
+  },
   //{
-   // name: "Instagram",
-    //url: "http://instagram.com",
-    //Icon: styleIcon(InstagramIcon),
+  // name: "Instagram",
+  //url: "http://instagram.com",
+  //Icon: styleIcon(InstagramIcon),
   //},
 ];
